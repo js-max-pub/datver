11fbc5fa0784a1538d2e03697b328bb187b2f382
Max Fechner <max.fechner@gmail.com>
2021-07-15 17:13:57 +0200

    log

--- logger.js
+++ logger.js
@@ -10,12 +10,12 @@ export class Logger {
 		const ip = this.request.headers.get("x-forwarded-for") ?? '';
 		// console.log("IP", ip)
 		//ip.padStart(16, ' '), 
-		let body = [x.limit.padStart(12, ' '), x.status, x.name.padEnd(8, ' '), x.path, JSON.stringify(Object.fromEntries(Array.from(this.request.headers)))].join('    ')
+		let body = [ip.padEnd(16, ' '), x.limit.padStart(12, ' '), x.status, x.name.padEnd(8, ' '), x.path, JSON.stringify(Object.fromEntries(Array.from(this.request.headers)))].join('    ')
 		console.log('body', body)
 		// var formData = new FormData();
 		// formData.append("test", 'jo');
 		// formData.append("message", 'body');
-		let text = await fetch(`https://api.max.pub/datver?message=`+btoa(body), {
+		let text = await fetch(`https://api.max.pub/datver?message=` + btoa(body), {
 			// method: 'POST',
 			// headers: {
 			// 	'Content-Type': 'multipart/form-data'
